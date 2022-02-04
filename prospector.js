a21e665bde1f815ec9af9e2af4dcb33576d7b275
Himanshu Raj <himanshurajverma549@gmail.com>
2022-02-04 20:58:09 +0530

    completed filter up for review

---
+++ prospector.js
@@ -0,0 +1,36 @@
+const prospectTagContainer = document.querySelectorAll("[data-block='tech']");
+const updateEle = document.querySelectorAll("[data-block='update']");
+class PROSPECTOR {
+    constructor(){
+        this.tagsArry = prospectTagContainer;
+        this.textArry = updateEle;
+        this.init();
+    }
+
+    init(){
+        // function to check and update the number of items present in the categories.
+        this.checkAndUpdate();
+    }
+
+    checkAndUpdate(){
+        /*loop through all cms headings -> get the number of tags present -> loop through left qualification crieria box ->
+        match with the heading update the number of tags.
+        */
+        this.tagsArry.forEach(tagItem => {
+           let checkHead = tagItem.querySelector(".prosp-tag-head").innerHTML;
+           let numberOfTags = [...tagItem.querySelectorAll(".prosp-tag")].filter(tag => !(tag.classList.contains("w-condition-invisible")));
+           this.textArry.forEach(item => {
+               let confirmHead = item.querySelector(".prosp-tag-head").innerHTML;
+                if(checkHead == confirmHead){
+                    let updateNumberOfTag = item.querySelector(".num-tag-span");
+                    updateNumberOfTag.innerHTML = numberOfTags.length;
+                }
+           })
+
+        })
+    }
+
+}
+if(prospectTagContainer && updateEle){
+    new PROSPECTOR(prospectTagContainer, updateEle);
+}
---
+++ sliceAndDice.js
@@ -0,0 +1,94 @@
+const checkBoxesCategory = document.querySelectorAll("[category]");
+
+class ADVANCESEARCH {
+    constructor() {
+        this.checkBoxesCategory = checkBoxesCategory;
+        this.clonedTag = document.querySelector(".filter-tag").cloneNode(true);
+        this.resetFilter = document.querySelectorAll(".adv-reset-link");
+        this.init();
+    }
+
+    init() {
+        this.addListener();
+        this.clearTagsOnLoad();
+    }
+    // add listener to checkboxes as well as rest button.
+    addListener() {
+        checkBoxesCategory.forEach(category => {
+            let checkBoxs = category.querySelectorAll("[type='checkbox']");
+            if (!checkBoxs) return;
+            checkBoxs.forEach(chkbx => {
+                chkbx.addEventListener("click", (e) => {
+                    let currCate = category.getAttribute("category");
+                    let value = e.target.value;
+                    let chkEle = e.target.parentElement.querySelector(".adv-check-tick");
+                    let isChecked = chkEle.classList.contains("w--redirected-checked");
+                    if (!isChecked) {
+                        chkEle.classList.add("w--redirected-checked");
+                        this.updateTags(currCate, value, true);
+                    }
+                    else {
+                        chkEle.classList.remove("w--redirected-checked");
+                        this.updateTags(currCate, value, false);
+                    }
+                })
+            })
+        })
+        // reset the filter.
+        this.resetFilter.forEach(resetBtn => {
+            resetBtn.addEventListener('click', () => {
+                document.querySelectorAll(".rm-filter").forEach(closebtn => closebtn.click());
+            })
+        })
+    }
+
+    // add or remove tags from their respective container as well as check the condition for visibility.
+    updateTags(currCate, value, update) {
+        let blockToUpdate = document.querySelector(`[update-category=${currCate}]`);
+        if (update) {
+            let newTag = this.clonedTag.cloneNode(true);
+            newTag.querySelector(".filter-tag-name").innerHTML = value;
+            newTag.setAttribute('tag', `${value}`);
+            newTag.querySelector(".rm-filter").addEventListener('click', (e) => {
+                let getCheckBox = e.target.parentElement.getAttribute("tag");
+                document.querySelector(`[value='${getCheckBox}']`).click();
+            })
+            blockToUpdate.appendChild(newTag);
+            this.showAndHideContainer(blockToUpdate);
+        }
+        else {
+            blockToUpdate.querySelector(`[tag='${value}']`).remove();
+            this.showAndHideContainer(blockToUpdate);
+        }
+    }
+
+    // clear all the tags on load.
+    clearTagsOnLoad() {
+        let tagsToRemove = document.querySelector(".adv-category-filter").querySelectorAll(".filter-tag");
+        tagsToRemove.forEach(tag => tag.remove());
+    }
+
+    // show/hide tags container as well as tags parent container based on the tags present.
+    showAndHideContainer(blockToUpdate) {
+        let mainWrapper = document.querySelector(".adv-category-filter");
+        let tagsLength = mainWrapper.querySelectorAll(".filter-tag").length;
+        if (tagsLength > 0) {
+            mainWrapper.classList.remove("hide-res");
+            if (blockToUpdate.childElementCount > 0) {
+                blockToUpdate.parentElement.classList.remove("hide-block");
+            }
+            else {
+                blockToUpdate.parentElement.classList.add("hide-block");
+            }
+        }
+        else {
+            mainWrapper.classList.add("hide-res");
+            blockToUpdate.parentElement.classList.add("hide-block");
+        }
+
+    }
+}
+
+if(checkBoxesCategory){
+    new ADVANCESEARCH(checkBoxesCategory)
+}
